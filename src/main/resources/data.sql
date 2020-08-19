54edb026ee0ee29bc9cc451fed9c164e24ae360f
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2020-08-19 09:30:43 -0300

    Adicionado dados fake para inicializacao do database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO customer (uuid, cpf, name, address_id, created_at, updated_at)
+VALUES ('4647765e-6491-47aa-82bd-c08f87439421', '12345678910', 'Tony Stark', null, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
