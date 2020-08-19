0ca1e8d3ae45f4cfd96200b96279d6851a187028
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2020-08-19 13:34:49 -0300

    Teste integracao para criar um customer

---
+++ src/test/java/com/rafaelhayashi/brasilprev/model/CustomerMock.java
@@ -0,0 +1,23 @@
+package com.rafaelhayashi.brasilprev.model;
+
+public class CustomerMock {
+
+    private String name;
+    private String cpf;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
