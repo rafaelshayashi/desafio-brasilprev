cae49623a4a06b2e35710c267d396effd430b4b1
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2020-08-19 22:30:53 -0300

    Finalizacao de teste para o CRUD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
 INSERT INTO address (id, country, state, city, street, postal_code, created_at, updated_at)
 VALUES (1, 'EUA', 'CA', 'Los Angeles', '10880 Malibu Point', '90265', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
+INSERT INTO address (id, country, state, city, street, postal_code, created_at, updated_at)
+VALUES (2, 'EUA', 'CA', 'Los Angeles', '20000 Malibu Point', '90266', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
 INSERT INTO customer (uuid, cpf, name, address_id, created_at, updated_at)
-VALUES ('4647765e-6491-47aa-82bd-c08f87439421', '12345678910', 'Tony Stark', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+VALUES ('4647765e-6491-47aa-82bd-c08f87439421', '004.005.006-10', 'Tony Stark', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO customer (uuid, cpf, name, address_id, created_at, updated_at)
+VALUES ('72c5d8c5-539a-42f0-8a01-5c4ec7cdb924', '005.001.009-10', 'Steve Rogers', 2, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
--- src/test/java/com/rafaelhayashi/brasilprev/CustomerApiDtoTests.java
+++ src/test/java/com/rafaelhayashi/brasilprev/CustomerApiDtoTests.java
@@ -35,13 +35,33 @@ public class CustomerApiDtoTests {
 
         CustomerMock customer = new CustomerMock();
         customer.setName("Gandalf The Gray");
-        customer.setCpf("09876543210");
+        customer.setCpf("001.002.003-10");
 
         String payload = objectMapper.writeValueAsString(customer);
         this.mvc.perform(MockMvcRequestBuilders.post("/api/v1/customers").contentType(MediaType.APPLICATION_JSON).content(payload))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("name", equalTo("Gandalf The Gray")))
-                .andExpect(MockMvcResultMatchers.jsonPath("cpf", equalTo("09876543210")))
+                .andExpect(MockMvcResultMatchers.jsonPath("cpf", equalTo("001.002.003-10")))
                 .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void shouldUpdateACustomer() throws Exception {
+        CustomerMock customer = new CustomerMock();
+        customer.setName("Tony (The Iron man) Stark");
+        customer.setCpf("011.022.033-10");
+
+        String payload = objectMapper.writeValueAsString(customer);
+        this.mvc.perform(MockMvcRequestBuilders.put("/api/v1/customers/4647765e-6491-47aa-82bd-c08f87439421").contentType(MediaType.APPLICATION_JSON).content(payload))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("name", equalTo("Tony (The Iron man) Stark")))
+                .andExpect(MockMvcResultMatchers.jsonPath("cpf", equalTo("011.022.033-10")))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void shouldDeleteACustomer() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders.delete("/api/v1/customers/72c5d8c5-539a-42f0-8a01-5c4ec7cdb924"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
