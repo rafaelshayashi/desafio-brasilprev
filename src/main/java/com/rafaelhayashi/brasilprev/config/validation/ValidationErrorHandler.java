29c41b593128372497d1f12b2a0c552de8f18689
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2020-08-19 22:22:00 -0300

    Complementando o handle de exceptions

--- src/main/java/com/rafaelhayashi/brasilprev/config/validation/ValidationErrorHandler.java
+++ src/main/java/com/rafaelhayashi/brasilprev/config/validation/ValidationErrorHandler.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -34,5 +38,25 @@ public class ValidationErrorHandler {
         String message = messageSource
                 .getMessage("MethodArgumentNotValidException.message", null, "Validation error", LocaleContextHolder.getLocale());
         return new MessageErrorDto(message, fieldErrorDtoStream.collect(Collectors.toList()));
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public MessageErrorDto handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        String message = messageSource
+                .getMessage("HttpMessageNotReadableException.message", null, "Required request body is missing", LocaleContextHolder.getLocale());
+        return new MessageErrorDto(message, new ArrayList<>());
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public MessageErrorDto handleInvalidArgumentException(IllegalArgumentException ex) {
+        return new MessageErrorDto(ex.getLocalizedMessage(), new ArrayList<>());
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public MessageErrorDto handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException ex) {
+        return new MessageErrorDto(ex.getLocalizedMessage(), new ArrayList<>());
     }
 }
